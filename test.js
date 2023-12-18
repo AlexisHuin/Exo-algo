36605af6e5df0bc81278b0b6c7fcddc894983eed
Alexis Huin <redemption41@gmail.com>
2023-12-18 15:39:51 +0100

    add fibo

--- test.js
+++ test.js
@@ -105,3 +105,17 @@ document.addEventListener("DOMContentLoaded", function () {
     return reverseString;
   }
 });
+
+// Fibonacci a implémenter
+function fib(n) {
+  if(n === 0) {
+    return 0
+  } else if( n === 1) {
+    return 1
+  } else {
+    
+    return fib(n - 1) + fib( n - 2)
+  }
+}
+
+
